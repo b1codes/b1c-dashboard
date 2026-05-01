33031a684a3f1e6f5a303195d757a83567d9daad
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2026-05-01 13:29:40 -0400

    feat(infra): add S3 bucket policies for CloudFront OAC access

--- infra/s3.tf
+++ infra/s3.tf
@@ -19,3 +19,49 @@ resource "aws_s3_bucket_versioning" "cms" {
     status = "Enabled"
   }
 }
+
+data "aws_iam_policy_document" "app_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.app.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.app.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "app" {
+  bucket = aws_s3_bucket.app.id
+  policy = data.aws_iam_policy_document.app_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "cms_bucket_policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.cms.arn}/*"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceArn"
+      values   = [aws_cloudfront_distribution.cms.arn]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "cms" {
+  bucket = aws_s3_bucket.cms.id
+  policy = data.aws_iam_policy_document.cms_bucket_policy.json
+}
